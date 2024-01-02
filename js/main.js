bc113d196ac848fbffefdb9dd84dffd28ec33316
Feshchenok Viktor <vituok.feschenok@gmail.com>
2024-01-02 16:41:57 -0500

    upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const chooseColor = document.querySelectorAll('.choose__color-btn');
+const chooseItem = document.querySelectorAll('.content-item');
+
+
+chooseColor.forEach(function(element){
+    element.addEventListener('click',open)
+})
+
+function open(evt){
+    const target = evt.currentTarget;
+
+chooseColor.forEach(function(item){
+    item.classList.remove('choose__color-btn--active')
+})
+
+    target.classList.add('choose__color-btn--active')
+}
