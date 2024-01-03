5fbc2625290f8f75eb4104f4164255410e668046
Feshchenok Viktor <vituok.feschenok@gmail.com>
2024-01-02 23:22:47 -0500

    JS choose color

--- js/main.js
+++ js/main.js
@@ -8,10 +8,20 @@ chooseColor.forEach(function(element){
 
 function open(evt){
     const target = evt.currentTarget;
+    const button = target.dataset.button;
+    const contentActive = document.querySelectorAll('.${button}');
 
-chooseColor.forEach(function(item){
-    item.classList.remove('choose__color-btn--active')
-})
+    chooseColor.forEach(function(item){
+        item.classList.remove('choose__color-btn--active')
+    })
 
     target.classList.add('choose__color-btn--active')
+
+    contantItem.forEach(function(item){
+        item.classList.remove('content-item__active')
+    });
+
+    contentActive.forEach(function(item){
+        item.classList.add('content-item__active')
+    })
 }
